7d43c72bb03d4f067573fc5b4051377aef2ba4e1
Martin Fracker <martin.frackerjr@gmail.com>
2017-10-15 20:33:49 -0500

    Clean up skills and fix typo

---
+++ components/Skills.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-row>
+    <skill class="col-md" name="Full Stack Development" icon="list-alt">
+      I have experience developing apps from the user to experience to data
+      storage. To this extent, I am skilled in several different technologies
+      including Ruby on Rails, Vue.js, Nuxt.js, Angular.js, ASP.NET Web API and
+      MVC, Node.js, MS SQL, and Postgres SQL.
+    </skill>
+    <skill class="col-md" name="Consulting" icon="handshake-o">
+      I have worked at Improving as a Software Consultant since April of 2016. For the
+      past year, worked on and put into production an enterprise loan origination
+      application that has originated over a half-billion dollars worth of
+      loans.</skill>
+    <skill class="col-md" name="DevOps" icon="wrench">
+      I know how to create Continuous Integration and Delivery pipelines using
+      Travis CI and Jenkins as well as how to work in a cross-functional Scrum
+      team to deliver quality code to the client frequently.
+    </skill>
+  </b-row>
+</template>
+
+<script>
+import Skill from './Skill'
+export default {
+  components: {
+    Skill
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,7 @@
     page-section(title="Software Developer")
       about-me
     page-section(color="#e3fdff")
-      b-row.skills
-        skill.col-md(name="Full Stack Development" icon="list-alt") I have experience developing apps from the user to experience to data storage. To this extent, I am skilled in several different technologies including Ruby on Rails, Vue.js, Nuxt.js Angular.js, ASP.NET Web API and MVC, Node.js, MS SQL, and Postgres SQL.
-        skill.col-md(name="Consulting" icon="handshake-o") Worked at Improving as a Software Consultant since April of 2016. For the past year, worked on and put into production an enterprise loan origination application that has originated over a half-billion dollars worth of loans.
-        skill.col-md(name="DevOps" icon="wrench") I know how to create Continuous Integration and Delivery pipelines using Travis CI and Jenkins as well as how to work in a cross-functional Scrum team to deliver quality code to the client frequently.
+      skills
     page-section(title="Contact Me")
       contact-me
 </template>
@@ -15,13 +12,13 @@
 import AboutMe from '~/components/AboutMe'
 import ContactMe from '~/components/ContactMe'
 import PageSection from '~/components/PageSection'
-import Skill from '~/components/Skill'
+import Skills from '~/components/Skills'
 export default {
   components: {
     AboutMe,
     ContactMe,
     PageSection,
-    Skill
+    Skills
   }
 }
 </script>
