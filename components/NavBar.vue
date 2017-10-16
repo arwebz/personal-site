28ef9be0d3c35d4346b39bc68c5e887f0ecf5d51
Martin Fracker <martin.frackerjr@gmail.com>
2017-10-15 20:54:49 -0500

    Prevent Home link from being active all the time

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,7 +4,7 @@
     b-navbar-brand(href="javascript:void(0)") Martin Fracker, Jr.
     b-collapse(is-nav id="nav_collapse")
       b-nav(is-nav-bar)
-        b-nav-item(to="/" :active="$route.name === 'index'") Home
+        b-nav-item(to="/" :exact="true") Home
         b-nav-item(to="/projects") Projects
         b-nav-item-dropdown#resume-nav(text="Resume" right)
           b-dropdown-item(href="https://github.com/Towerism/resume/raw/master/fracker-martin-resume.pdf") PDF
