a5d70fd1877b236f5e2386142a8542819b013e64
Martin Fracker <martin.frackerjr@gmail.com>
2017-10-15 20:10:50 -0500

    Change wording of DevOps skill description

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
       b-row.skills
         skill.col-md(name="Full Stack Development" icon="list-alt") I have experience developing apps from the user to experience to data storage. To this extent, I am skilled in several different technologies including Ruby on Rails, Vue.js, Nuxt.js Angular.js, ASP.NET Web API and MVC, Node.js, MS SQL, and Postgres SQL.
         skill.col-md(name="Consulting" icon="handshake-o") Worked at Improving as a Software Consultant since April of 2016. For the past year, worked on and put into production an enterprise loan origination application that has originated over a half-billion dollars worth of loans.
-        skill.col-md(name="DevOps" icon="wrench") I have experience creating Continuous Integration and Delivery pipelines using Travis CI and Jenkins as well as working in a cross-functional Scrum team to deliver quality code to the client frequently.
+        skill.col-md(name="DevOps" icon="wrench") I know how to create Continuous Integration and Delivery pipelines using Travis CI and Jenkins as well as how to work in a cross-functional Scrum team to deliver quality code to the client frequently.
     page-section(title="Contact Me")
       contact-me
     page-section.text-center(color="#ffefd3")
