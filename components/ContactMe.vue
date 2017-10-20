ec19e4bcda8f642dd3fc0689875fe36e0a6b71a5
Martin Fracker <martin.frackerjr@gmail.com>
2017-10-20 00:18:03 -0500

    Rework form fields and submissions to be mailgun compatible

--- components/ContactMe.vue
+++ components/ContactMe.vue
@@ -3,17 +3,18 @@
     p If you have any questions about my skills, any of the projects I have worked on, or would like to set up an interview, please send me an email using the form below.
     form(@submit.prevent="onSubmit" :action="action" :name="formName" netlify)
       input(type="hidden" v-model="honeypot")
+      input(type="hidden" name="to" v-model="form.to")
       .form-row
         .form-group.col-md
-          input.form-control(name="fromName" type="text" v-model="form.fromName" placeholder="Name" required)
+          input.form-control(type="text" v-model="form.name" placeholder="Name" required)
         .form-group.col-md
-          input.form-control(name="email" type="email" v-model="form.email" placeholder="Email" required)
+          input.form-control(name="from" type="email" v-model="form.email" placeholder="Email" required)
       .form-row
         .form-group.col-md
           input.form-control(name="subject" type="text" v-model="form.subject" placeholder="Subject" required)
       .form-row
         .form-group.col
-          textarea#message.form-control(name="message" type="text" v-model="form.message" placeholder="Message" required)
+          textarea#message.form-control(name="text" type="text" v-model="form.text" placeholder="Message" required)
       .form-row
         .col
           spinner-button.float-right(:disabled="isLoading || status === true" :isLoading="isLoading" :status="status")
@@ -34,10 +35,10 @@ export default {
   data () {
     return {
       form: {
-        fromName: '',
+        name: '',
         email: '',
         subject: '',
-        message: ''
+        text: ''
       },
       action: 'contact',
       formName: 'contact',
@@ -46,12 +47,23 @@ export default {
       status: ''
     }
   },
+  computed: {
+    submissionPayload () {
+      return {
+        'form-name': this.formName,
+        from: `${this.form.name} <${this.form.email}>`,
+        to: 'martin.frackerjr@gmail.com',
+        subject: this.form.subject,
+        text: this.form.text
+      }
+    }
+  },
   methods: {
     onSubmit () {
       if (this.honeypot !== '') {
         return
       }
-      let formData = querystring.stringify({ 'form-name': this.formName, ...this.form })
+      let formData = querystring.stringify(this.submissionPayload)
       this.isLoading = true
       axios.post(this.action, formData).then(() => {
         this.isLoading = false
