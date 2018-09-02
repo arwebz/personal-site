127964670344072de3292b5f0259c6253e3bfb8e
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-09-02 11:43:13 -0500

    Ensure dates are displayed in proper order

--- helpers/formatProjectDate.js
+++ helpers/formatProjectDate.js
@@ -5,9 +5,9 @@ const OUT_FORMAT = 'MMMM YYYY'
 
 export function formatProjectDate (project) {
   const dates = {
-    start: moment(project.end, IN_FORMAT).format(OUT_FORMAT),
+    start: moment(project.start, IN_FORMAT).format(OUT_FORMAT),
     end: project.end
-      ? moment(project.start, IN_FORMAT).format(OUT_FORMAT)
+      ? moment(project.end, IN_FORMAT).format(OUT_FORMAT)
       : 'present'
   }
   return `${dates.start} to ${dates.end}`
