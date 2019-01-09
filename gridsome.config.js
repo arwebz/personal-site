304b580ed642c82bcba116135f931cfb7058d5b0
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2019-01-08 20:44:26 -0600

    Add gridsome

---
+++ gridsome.config.js
@@ -0,0 +1,7 @@
+// This is where project configuration and installed plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: []
+}
