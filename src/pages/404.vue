7118c7b7710e5ea90d2beb6e8d74e39c041855fc
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2019-02-28 13:02:19 -0600

    Add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  section.main.style1.fullscreen
+    .content
+      header
+        h2 404 Not Found
+      p
+
+        | Whoops! The page you're looking for was either moved or deleted.
+
+      footer
+        ul.actions.special
+          li
+            g-link.button(to="/") Take Me Home
+</template>
