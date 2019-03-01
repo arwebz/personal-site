6ffe7905448dd022e1b3d628776693f482dbd4c5
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2019-02-28 20:53:40 -0600

    404: remove the link

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -6,7 +6,7 @@
           h2 404 Not Found
         p
           | Whoops! The page you're looking for was either moved or deleted.
-        footer
+        //footer
           ul.actions.special
             li
               g-link.button(to="/") Take Me Home
