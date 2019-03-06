8e5462bf07819343e3c7f87a383372703a4c0d00
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2019-03-06 14:35:32 -0600

    social callout: tweak styling

--- src/components/CallOut.vue
+++ src/components/CallOut.vue
@@ -1,11 +1,16 @@
 <template lang="pug">
   #callout
-    h3 What did you think?
-    p Share this post on social media, or join the discussion below!
+    strong What did you think?
+    |
+    | Share this post on social media, or join the discussion below!
 </template>
 
-<style scoped>
-.margin-diminshed {
+<style lang="scss" scoped>
+#callout {
+  padding-right: 1em;
+}
+
+.no-margin {
   margin: 0;
 }
 </style>
